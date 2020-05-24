9f730f5c6c2e75f077adb710fc04ba7a02710ac9
hrithik098 <hrithiky328@gmail.com>
2020-05-24 13:55:09 +0530

    First successful build of the project

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include "mgos.h"
+#include "mgos_mcp2515.h"
+
+struct MCP_CAN *mc;
+
+
+void can_send(void *arg) {
+  unsigned char message[2] = {0, 1};
+  int id = mgos_mcp2515_getCanId(mc);
+  mgos_mcp2515_sendMsgBuf(mc, id, 0, 2, message);
+  
+}
+
+void can_recieve(void *arg)
+{
+  unsigned char len = 0;
+  unsigned char buf[8];
+  if (CAN_MSGAVAIL == mgos_mcp2515_checkReceive(mc))
+  {
+    LOG(LL_INFO, ("TCU: CAN message received"));
+    mgos_mcp2515_readMsgBuf(mc, &len, buf); // message is read in buf, to see message print the buf
+  }
+  else
+  {
+    LOG(LL_INFO, ("TCU: No CAN message received"));
+  }
+}
+extern "C"
+{
+  enum mgos_app_init_result mgos_app_init(void)
+  {
+    LOG(LL_INFO, ("TCU: Adding CAN interface."));
+    mc = mgos_mcp2515_create(10); // cs pin as input
+    while (CAN_OK != mgos_mcp2515_begin(mc, CAN_500KBPS))
+    {
+      LOG(LL_INFO, ("TCU: CAN init failed"));
+    }
+
+    LOG(LL_INFO, ("TCU: CAN init ok!"));
+
+    return MGOS_APP_INIT_SUCCESS;
+  }
+}
