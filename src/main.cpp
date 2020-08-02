ce0f29c017dae007f6696c066529bc218dd25428
hrithik098 <hrithiky328@gmail.com>
2020-08-02 16:25:56 +0530

    Added spi config

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include "mgos.h"
 #include "mgos_mcp2515.h"
-
-struct MCP_CAN *mc; 
-
+#include "mgos_spi.h"
 
 /*! \mainpage MCP2515 testing
  *
@@ -10,6 +8,9 @@ struct MCP_CAN *mc;
  *
  * This is a test app to communicate to CAN transceiver. 
  */
+
+struct MCP_CAN *mc;
+
 
 
 void can_send(void *arg)
@@ -21,20 +22,22 @@ void can_send(void *arg)
 /**< Send message to CAN transciever */
 
 
+
 void can_recieve(void *arg)
 {
+
   unsigned char len = 0;
   unsigned char buf[8];
   if (CAN_MSGAVAIL == mgos_mcp2515_checkReceive(mc))
   {
     LOG(LL_INFO, ("TCU: CAN message received"));
     mgos_mcp2515_readMsgBuf(mc, &len, buf); // message is read in buf, to see message print the buf
-                                            
   }
   else
   {
     LOG(LL_INFO, ("TCU: No CAN message received"));
   }
+
 }
 /**< Recieve messages from other CAN controller */
 
@@ -43,6 +46,16 @@ extern "C"
   enum mgos_app_init_result mgos_app_init(void)
   {
     LOG(LL_INFO, ("TCU: Adding CAN interface."));
+
+    struct mgos_spi *spi;
+    
+    spi = mgos_spi_get_global();
+
+    if (spi == NULL)
+    {
+      LOG(LL_ERROR, ("SPI is not configured, make sure spi.enable is true"));
+    }
+
     mc = mgos_mcp2515_create(10); // cs pin as input
     while (CAN_OK != mgos_mcp2515_begin(mc, CAN_500KBPS))
     {
